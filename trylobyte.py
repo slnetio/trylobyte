5dc662de757a4676a1923dd8c0d0e724d1c7c9f5
Jake Katrakis Larkin <jkl@slnet.io>
2024-05-01 00:16:19 +1000

    Moved app logic to avoid circular import

--- trylobyte.py
+++ trylobyte.py
@@ -1,15 +1,3 @@
-import os
-import vultr_api
-from dotenv import load_dotenv
+import main_menu
 
-load_dotenv()
-
-vultr_url = os.getenv('vultr_url')
-vultr_key = os.getenv('vultr_key')
-
-account_data = vultr_api.get(vultr_url, 'account', vultr_key)
-instance_data = vultr_api.get(vultr_url, 'instances', vultr_key)
-
-print(account_data['account']['name'])
-print(instance_data['instances'][0]['id'])
-print(instance_data['instances'][0]['label'])
+main_menu.main_menu()
